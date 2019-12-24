9feb36c21aef457ecb8c6fe2ee075c713edae748
Henrik Thoroe <henrik@thoroee.com>
2019-12-24 12:18:05 +0100

    Initial Commit

---
+++ SmartPaper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartPaper
+//
+//  Created by Henrik Thoroe on 24.12.19.
+//  Copyright © 2019 Henrik Thoroe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
